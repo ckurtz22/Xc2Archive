d5f43c9ec683bd8b76710c5b81aeecb2dcde8136
ckurtz22 <connorkurtz22@gmail.com>
2019-01-16 16:36:36 -0800

    started work on bdat stuff

---
+++ Xc2Archive/Bdat/BdatFile.cs
@@ -0,0 +1,29 @@
+﻿using LibHac.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xc2Archive.Bdat;
+
+namespace Xc2Archive.Bdat
+{
+	class BdatFile
+	{
+		public List<BdatTable> Tables = new List<BdatTable>();
+		public BdatFile(IFile bdat)
+		{
+			using (var reader = new BinaryReader(bdat.AsStream()))
+			{
+				var tableCount = reader.ReadInt32();
+				var fileLength = reader.ReadInt32();
+				for (int i = 0; i < tableCount; i++)
+				{
+					reader.BaseStream.Position = 8 + (4 * i);
+					reader.BaseStream.Position = reader.ReadInt32();
+					Tables.Add(new BdatTable(reader));
+				}
+
+			}
+		}
+	}
+}
---
+++ Xc2Archive/Bdat/BdatTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Xc2Archive.Bdat
+{
+	class BdatTable
+	{
+		public long FileOffset { get; }
+		public ushort Flags { get; }
+		public ushort NameOffset { get; }
+		public ushort ItemSize { get; }
+		public ushort HashTableOffset { get; }
+		public ushort ItemTableOffset { get; }
+		public ushort ItemCount { get; }
+		public ushort BaseId { get; }
+		public ushort field_14 { get; }
+		public ushort Checksum { get; }
+		public int StringsOffset { get; }
+		public int StringsLength { get; }
+		public ushort MemberTableOffset { get; }
+		public ushort MemberCount { get; }
+
+
+		public BdatTable(BinaryReader reader)
+		{
+			FileOffset = reader.BaseStream.Position;
+			if(reader.ReadChars(4).ToString() != "BDAT")
+			{
+				Console.WriteLine("Tried reading bdat table that wasn't a bdat table");
+				return;
+			}
+			Flags = reader.ReadUInt16();
+			NameOffset = reader.ReadUInt16();
+			ItemSize = reader.ReadUInt16();
+			HashTableOffset = reader.ReadUInt16();
+			ItemTableOffset = reader.ReadUInt16();
+			ItemCount = reader.ReadUInt16();
+			BaseId = reader.ReadUInt16();
+			field_14 = reader.ReadUInt16();
+			Checksum = reader.ReadUInt16();
+			StringsOffset = reader.ReadInt32();
+			StringsLength = reader.ReadInt32();
+			MemberTableOffset = reader.ReadUInt16();
+			MemberCount = reader.ReadUInt16();
+			//read filename at NameOffset + FileOffset
+			//read bdat members
+
+
+
+		}
+	}
+}
